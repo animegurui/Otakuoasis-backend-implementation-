f06e2b34080c2f1212cf49c5e959fe428827ac09
animegurui <favorsmith8422@gmail.com>
2025-08-25 10:26:32 +0100

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Token missing' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.userId, role: decoded.role };
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token', err });
+  }
+};
